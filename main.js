235d0ac52cd248159f2dac7acaadb248938165c2
Bilostenko <bilostenkod@gmail.com>
2023-01-25 21:12:25 +0200

    add logic

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 
 const number = document.querySelectorAll('.number')
 const inp = document.querySelector('.inp')
+const sign = document.querySelectorAll('.sign')
+const result = document.querySelector('.result')
+const reset = document.querySelector('.reset')
 
+/* enter number */
 for (let i = 0; i < number.length; ++i){
   number[i].addEventListener('click', select)
 }
-
-function select(){
-  inp.value = this.innerHTML
+/* enter sign */
+for (let i = 0; i < number.length; ++i){
+  sign[i].addEventListener('click', select)
 }
+function select(){
+  inp.value += this.innerHTML
+}
+
+/*get result */
+/* result.addEventListener('click', mathOperation)
+function mathOperation(){
+  inp.value  
+} */
+
+/* reset entered data */
+/* reset.addEventListener('click', reset)
+function reset(){
+  inp.reset()
+} */
+
+
