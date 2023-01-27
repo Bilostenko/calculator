47f5c67df19cff4003a0745a2162e0c40b369fbd
Bilostenko <bilostenkod@gmail.com>
2023-01-27 16:34:52 +0200

    add functions

--- main.js
+++ main.js
@@ -4,47 +4,75 @@ const output = document.querySelector('.inp')
 const signBtns = document.querySelectorAll('.sign')
 const resultBtn = document.querySelector('.result')
 const resetBtn = document.querySelector('.reset')
+const negativeBtn = document.querySelector('.negative')
+const percentBtn = document.querySelector('.percent')
 output.value = 0
+
 /* enter number */
-for (let i = 0; i < numberBtns.length; ++i){
+for (let i = 0; i < numberBtns.length; ++i) {
   numberBtns[i].addEventListener('click', handleNumbers)
 }
+function handleNumbers() {
+  if (output.value == "0") {
+    output.value = this.innerHTML;
+  } else if (output.value.endsWith("+") || output.value.endsWith("-") || output.value.endsWith("*") || output.value.endsWith("/")) {
+    if (output.value.endsWith("0")) {
+      output.value = output.value.slice(0, -1) + this.innerHTML;
+    } else {
+      output.value += this.innerHTML;
+    }
+  } else {
+    output.value += this.innerHTML;
+  }
+}
+
 /* enter sign */
-for (let j = 0; j < signBtns.length; ++j){
+for (let j = 0; j < signBtns.length; ++j) {
   signBtns[j].addEventListener('click', handleOperators)
 }
+function handleOperators() {
+  if (!isNaN(output.value.slice(-1)) || output.value === "") {
+    output.value += this.innerHTML;
+  } else if (this.innerHTML === "-" && output.value.slice(-1) === "-") {
+    output.value = output.value.slice(0, -1);
+  } else if (this.innerHTML === "-" && output.value.slice(-1) === "+") {
+    output.value = output.value.slice(0, -1);
+    output.value += this.innerHTML;
+  } else if (this.innerHTML === "+" && output.value.slice(-1) === "+") {
+    output.value = output.value.slice(0, -1);
+  } else if (this.innerHTML === "*" && output.value.slice(-1) === "*") {
+    output.value = output.value.slice(0, -1);
+  } else if (this.innerHTML === "/" && output.value.slice(-1) === "/") {
+    output.value = output.value.slice(0, -1);
+  } else if (this.innerHTML === "%" && output.value.slice(-1) === "%") {
+    output.value = output.value.slice(0, -1);
+  }
+}
+
 /*get result */
 resultBtn.addEventListener('click', mathOperationResult)
-function mathOperationResult(){
-  output.value = eval(output.value) 
+function mathOperationResult() {
+  output.value = eval(output.value)
 }
+
 /* reset entered data */
 resetBtn.addEventListener('click', clear)
-function clear(){
+function clear() {
   output.value = 0
 }
 
-function handleOperators(){
-  // different sign&sign ending
-  // same sign&sign ending
-  // number ending
-  // empty input.value
-  if (output.value.at(-1) != "/") { 
-    output.value += this.innerHTML
-  } /* else if (){ 
-
-  } else if (){ 
-
-  } else { // 
-
-  } */
+/* change number to negative number */
+negativeBtn.addEventListener('click', negative)
+function negative() {
+  if (output.value == 0) {
+    output.value = "-" + this.innerHTML
+  } else if (output.value.endsWith("+") || output.value.endsWith("-") || output.value.endsWith("*") || output.value.endsWith("/")) {
+    output.value += "-" + this.innerHTML
+  }
 }
 
-function handleNumbers(){
-  // input.value = 0 - replace 0 by number
-  // input.value != 0 - add number
-  // input.value ends with sign and 0 = replace by number
-/*   if ()
+/* perform an action with a % sign */
+percentBtn.addEventListener('click', percent)
+function percent() {
 
-*/
 }
