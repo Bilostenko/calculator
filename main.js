e1e5c918b8bfc0443043b82cba2414873e7f2639
Bilostenko <bilostenkod@gmail.com>
2023-01-27 11:02:31 +0200

    add js

--- main.js
+++ main.js
@@ -30,3 +30,39 @@ function clear(){
 }
 
 
+/*------------------*/
+
+const number = document.querySelectorAll('.number')
+const inp = document.querySelector('.inp')
+const sign = document.querySelectorAll('.sign')
+const result = document.querySelector('.result')
+const reset = document.querySelector('.reset')
+
+/* enter number */
+for (let i = 0; i < number.length; ++i){
+  number[i].addEventListener('click', selectNumber)
+}
+/* enter sign */
+for (let i = 0; i < sign.length; ++i){
+  sign[i].addEventListener('click', selectSign)
+}
+
+function selectNumber(){
+  inp.value += this.innerHTML
+}
+
+function selectSign(){
+  inp.value += this.innerHTML
+}
+
+/*get result */
+result.addEventListener('click', mathOperationResult)
+function mathOperationResult(){
+  inp.value = eval(inp.value) 
+}
+
+/* reset entered data */
+reset.addEventListener('click', clear)
+function clear(){
+  inp.value= ""
+}
